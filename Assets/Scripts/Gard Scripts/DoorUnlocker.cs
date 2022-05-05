40588cdd09911db829a7a1ca965e0f88f1c9d15e
Gardathor <gard.kvilvang@gmail.com>
2022-05-05 11:52:19 +0200

    gard stuff

--- Assets/Scripts/Gard Scripts/DoorUnlocker.cs
+++ Assets/Scripts/Gard Scripts/DoorUnlocker.cs
@@ -8,11 +8,23 @@ public class DoorUnlocker : MonoBehaviour
     public PressurePlate _PressurePlateBox;
     public PressurePlatePlayer _PressurePlatePlayer;
 
+    [SerializeField] private Animator door;
+    //[SerializeField] private Animator door2;
+    //[SerializeField] private Animator door3;
+    //[SerializeField] private Animator door4;
+
+    [HideInInspector] public bool doorUnlocked;
+    
+
     void Update()
     {
         if (_PressurePlatePlayer.pressureplateActivatedToggled)
         {
-            gameObject.SetActive(false);
+            //gameObject.SetActive(false);
+            door.Play("FirstDoor");
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            print("door ublocked");
+
         }
 
         if (_PressurePlateBox.pressureplateActivatedHold)
