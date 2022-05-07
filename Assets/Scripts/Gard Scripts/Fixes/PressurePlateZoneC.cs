a445ba5f93551e7c7811feedb616cc9ad70c67da
Gardathor <gard.kvilvang@gmail.com>
2022-05-07 18:00:05 +0200

    change c

--- Assets/Scripts/Gard Scripts/Fixes/PressurePlateZoneC.cs
+++ Assets/Scripts/Gard Scripts/Fixes/PressurePlateZoneC.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,35 @@ public class PressurePlateZoneC : MonoBehaviour
     [SerializeField] private Animator _Animator;
 
     [HideInInspector] public bool pressureplateActivatedHold;
+
+    [SerializeField] private GameObject Flame1;
+    [SerializeField] private GameObject Flame2;
+
+    private bool flame1Active;
+    private bool flame2Acrive;
+    
+    
+
+    private void Start()
+    {
+        Flame1.SetActive(false);
+        Flame2.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (pressureplateActivatedHold)
+        {
+            Flame1.SetActive(true);
+            flame1Active = true;
+        }
+        else if (!pressureplateActivatedHold)
+        {
+            Flame1.SetActive(false);
+            flame1Active = false;
+        }
+    }
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
